610f4e575bdfde5380c2b746114b32a496d63722
xubuntu <xubuntu@xubuntu-VirtualBox.(none)>
2017-04-01 14:19:17 +0300

    First  commit on the new istance of the simulator. Change from the original OverSim code are on the CHord, DHT and DHTTestAPP.I am in the stage that i send from the overlay to upper tier the time that a node needs to get ready.

---
+++ src/applications/test/TestMessage_m.cc
@@ -0,0 +1,299 @@
+//
+// Generated file, do not edit! Created by opp_msgc 4.2 from applications/test/TestMessage.msg.
+//
+
+// Disable warnings about unused variables, empty switch stmts, etc:
+#ifdef _MSC_VER
+#  pragma warning(disable:4101)
+#  pragma warning(disable:4065)
+#endif
+
+#include <iostream>
+#include <sstream>
+#include "TestMessage_m.h"
+
+// Template rule which fires if a struct or class doesn't have operator<<
+template<typename T>
+std::ostream& operator<<(std::ostream& out,const T&) {return out;}
+
+// Another default rule (prevents compiler from choosing base class' doPacking())
+template<typename T>
+void doPacking(cCommBuffer *, T& t) {
+    throw cRuntimeError("Parsim error: no doPacking() function for type %s or its base class (check .msg and _m.cc/h files!)",opp_typename(typeid(t)));
+}
+
+template<typename T>
+void doUnpacking(cCommBuffer *, T& t) {
+    throw cRuntimeError("Parsim error: no doUnpacking() function for type %s or its base class (check .msg and _m.cc/h files!)",opp_typename(typeid(t)));
+}
+
+
+
+
+EXECUTE_ON_STARTUP(
+    cEnum *e = cEnum::find("MessageType");
+    if (!e) enums.getInstance()->add(e = new cEnum("MessageType"));
+    e->insert(MYMSG_PING, "MYMSG_PING");
+    e->insert(MYMSG_PONG, "MYMSG_PONG");
+);
+
+Register_Class(TestMessage);
+
+TestMessage::TestMessage(const char *name, int kind) : cPacket(name,kind)
+{
+    this->type_var = 0;
+}
+
+TestMessage::TestMessage(const TestMessage& other) : cPacket(other)
+{
+    copy(other);
+}
+
+TestMessage::~TestMessage()
+{
+}
+
+TestMessage& TestMessage::operator=(const TestMessage& other)
+{
+    if (this==&other) return *this;
+    cPacket::operator=(other);
+    copy(other);
+    return *this;
+}
+
+void TestMessage::copy(const TestMessage& other)
+{
+    this->type_var = other.type_var;
+    this->senderAddress_var = other.senderAddress_var;
+}
+
+void TestMessage::parsimPack(cCommBuffer *b)
+{
+    cPacket::parsimPack(b);
+    doPacking(b,this->type_var);
+    doPacking(b,this->senderAddress_var);
+}
+
+void TestMessage::parsimUnpack(cCommBuffer *b)
+{
+    cPacket::parsimUnpack(b);
+    doUnpacking(b,this->type_var);
+    doUnpacking(b,this->senderAddress_var);
+}
+
+int TestMessage::getType() const
+{
+    return type_var;
+}
+
+void TestMessage::setType(int type)
+{
+    this->type_var = type;
+}
+
+TransportAddress& TestMessage::getSenderAddress()
+{
+    return senderAddress_var;
+}
+
+void TestMessage::setSenderAddress(const TransportAddress& senderAddress)
+{
+    this->senderAddress_var = senderAddress;
+}
+
+class TestMessageDescriptor : public cClassDescriptor
+{
+  public:
+    TestMessageDescriptor();
+    virtual ~TestMessageDescriptor();
+
+    virtual bool doesSupport(cObject *obj) const;
+    virtual const char *getProperty(const char *propertyname) const;
+    virtual int getFieldCount(void *object) const;
+    virtual const char *getFieldName(void *object, int field) const;
+    virtual int findField(void *object, const char *fieldName) const;
+    virtual unsigned int getFieldTypeFlags(void *object, int field) const;
+    virtual const char *getFieldTypeString(void *object, int field) const;
+    virtual const char *getFieldProperty(void *object, int field, const char *propertyname) const;
+    virtual int getArraySize(void *object, int field) const;
+
+    virtual std::string getFieldAsString(void *object, int field, int i) const;
+    virtual bool setFieldAsString(void *object, int field, int i, const char *value) const;
+
+    virtual const char *getFieldStructName(void *object, int field) const;
+    virtual void *getFieldStructPointer(void *object, int field, int i) const;
+};
+
+Register_ClassDescriptor(TestMessageDescriptor);
+
+TestMessageDescriptor::TestMessageDescriptor() : cClassDescriptor("TestMessage", "cPacket")
+{
+}
+
+TestMessageDescriptor::~TestMessageDescriptor()
+{
+}
+
+bool TestMessageDescriptor::doesSupport(cObject *obj) const
+{
+    return dynamic_cast<TestMessage *>(obj)!=NULL;
+}
+
+const char *TestMessageDescriptor::getProperty(const char *propertyname) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    return basedesc ? basedesc->getProperty(propertyname) : NULL;
+}
+
+int TestMessageDescriptor::getFieldCount(void *object) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    return basedesc ? 2+basedesc->getFieldCount(object) : 2;
+}
+
+unsigned int TestMessageDescriptor::getFieldTypeFlags(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldTypeFlags(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    static unsigned int fieldTypeFlags[] = {
+        FD_ISEDITABLE,
+        FD_ISCOMPOUND,
+    };
+    return (field>=0 && field<2) ? fieldTypeFlags[field] : 0;
+}
+
+const char *TestMessageDescriptor::getFieldName(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldName(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    static const char *fieldNames[] = {
+        "type",
+        "senderAddress",
+    };
+    return (field>=0 && field<2) ? fieldNames[field] : NULL;
+}
+
+int TestMessageDescriptor::findField(void *object, const char *fieldName) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    int base = basedesc ? basedesc->getFieldCount(object) : 0;
+    if (fieldName[0]=='t' && strcmp(fieldName, "type")==0) return base+0;
+    if (fieldName[0]=='s' && strcmp(fieldName, "senderAddress")==0) return base+1;
+    return basedesc ? basedesc->findField(object, fieldName) : -1;
+}
+
+const char *TestMessageDescriptor::getFieldTypeString(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldTypeString(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    static const char *fieldTypeStrings[] = {
+        "int",
+        "TransportAddress",
+    };
+    return (field>=0 && field<2) ? fieldTypeStrings[field] : NULL;
+}
+
+const char *TestMessageDescriptor::getFieldProperty(void *object, int field, const char *propertyname) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldProperty(object, field, propertyname);
+        field -= basedesc->getFieldCount(object);
+    }
+    switch (field) {
+        case 0:
+            if (!strcmp(propertyname,"enum")) return "MessageType";
+            return NULL;
+        default: return NULL;
+    }
+}
+
+int TestMessageDescriptor::getArraySize(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getArraySize(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    TestMessage *pp = (TestMessage *)object; (void)pp;
+    switch (field) {
+        default: return 0;
+    }
+}
+
+std::string TestMessageDescriptor::getFieldAsString(void *object, int field, int i) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldAsString(object,field,i);
+        field -= basedesc->getFieldCount(object);
+    }
+    TestMessage *pp = (TestMessage *)object; (void)pp;
+    switch (field) {
+        case 0: return long2string(pp->getType());
+        case 1: {std::stringstream out; out << pp->getSenderAddress(); return out.str();}
+        default: return "";
+    }
+}
+
+bool TestMessageDescriptor::setFieldAsString(void *object, int field, int i, const char *value) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->setFieldAsString(object,field,i,value);
+        field -= basedesc->getFieldCount(object);
+    }
+    TestMessage *pp = (TestMessage *)object; (void)pp;
+    switch (field) {
+        case 0: pp->setType(string2long(value)); return true;
+        default: return false;
+    }
+}
+
+const char *TestMessageDescriptor::getFieldStructName(void *object, int field) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldStructName(object, field);
+        field -= basedesc->getFieldCount(object);
+    }
+    static const char *fieldStructNames[] = {
+        NULL,
+        "TransportAddress",
+    };
+    return (field>=0 && field<2) ? fieldStructNames[field] : NULL;
+}
+
+void *TestMessageDescriptor::getFieldStructPointer(void *object, int field, int i) const
+{
+    cClassDescriptor *basedesc = getBaseClassDescriptor();
+    if (basedesc) {
+        if (field < basedesc->getFieldCount(object))
+            return basedesc->getFieldStructPointer(object, field, i);
+        field -= basedesc->getFieldCount(object);
+    }
+    TestMessage *pp = (TestMessage *)object; (void)pp;
+    switch (field) {
+        case 1: return (void *)(&pp->getSenderAddress()); break;
+        default: return NULL;
+    }
+}
+
+
